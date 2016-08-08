cf63addfa4cb5b82d886e876f16bc6cf0165f175
Ruth John <Rumyra@Ruths-MacBook.local>
2016-08-08 14:10:26 +0100

    sounds

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # MIDI-demos
+
 A set of cool interactive thangs to demonstrate the awesome power of Web MIDI
+
+### People who I couldn't have done this without and I thank very much
+
+##### General
+
+- [Daniel Mies](https://twitter.com/danielmies)
+
+##### Piano Demo
+
+- [Max Glenister](https://twitter.com/omgmog)
+- [Jake Archibald](https://twitter.com/jaffathecake)
+
+##### Sequencer
--- server.js
+++ server.js
@@ -10,14 +10,42 @@ app.use(express.static('public'));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-app.engine('html', htmling.express(__dirname + '/views/'));
+app.engine('html', htmling.express(__dirname + '/views/', {watch:true}));
 app.set('view engine', 'html');
 
 app.get('/', (req, res) => {
   res.render('index');
 });
+// play midi
 app.get('/piano', (req, res) => {
   res.render('piano');
+});
+// phone view
+app.get('/minim', (req, res) => {
+  res.render('minim');
+});
+// big screen
+app.get('/stepseq', (req, res) => {
+  // var fullUrl = req.protocol + '://' + req.get('host') + req.originalUrl;
+  var fullUrl = req.protocol + '://' + req.get('host') + '/minim';
+  req.JOIN_URL = fullUrl;
+  res.render('crowdsynth');
+});
+
+var pusher = new Pusher({
+  appId: process.env.PUSHID,
+  key: process.env.PUSHKEY,
+  secret: process.env.PUSHSEC
+});
+pusher.port = 443;
+
+
+app.post('/pusher/auth', function(req, res) {
+  console.log('auth called', req.body);
+  var socketId = req.body.socket_id;
+  var channel = req.body.channel_name;
+  var auth = pusher.authenticate(socketId, channel);
+  res.send(auth);
 });
 
 app.listen(process.env.PORT || 3001);
