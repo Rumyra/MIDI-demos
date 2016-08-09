c94986bd964065e5ccd4fb509d9a81b8a0c9ece8
Ruth John <Rumyra@Ruths-MacBook.local>
2016-08-09 21:01:10 +0100

    🙃

--- server.js
+++ server.js
@@ -30,23 +30,34 @@ app.get('/piano', (req, res) => {
 });
 // phone view
 app.get('/minim', (req, res) => {
-  res.render('minim');
+  var thisUrl = req.protocol + '://' + req.get('host');
+  req.THIS_URL = thisUrl;
+  req.PUSHKEY = process.env.PUSHKEY;
+  res.render('minim', req);
 });
 // big screen
 app.get('/stepseq', (req, res) => {
   // var fullUrl = req.protocol + '://' + req.get('host') + req.originalUrl;
-  var fullUrl = req.protocol + '://' + req.get('host') + '/minim';
-  req.JOIN_URL = fullUrl;
+  var thisUrl = req.protocol + '://' + req.get('host');
+  req.THIS_URL = thisUrl;
+  req.JOIN_URL = thisUrl+'/minim';
   req.PUSHKEY = process.env.PUSHKEY;
   res.render('crowdsynth', req);
 });
 
 app.post('/pusher/auth', function(req, res) {
-  console.log('auth called', req.body);
-  var socketId = req.body.socket_id;
-  var channel = req.body.channel_name;
-  var auth = pusher.authenticate(socketId, channel);
-  res.send(auth);
+  const socket_id = req.body.socket_id;
+  const channel_name = req.body.channel_name;
+  if(socket_id && channel_name) {
+    const userID = socket_id.replace(/\./g, "")+Date.now();
+    res.send(pusher.authenticate(
+      socket_id,
+      channel_name,
+      {user_id: userID, user_info: {}}
+    ))
+  } else {
+    res.sendStatus(401)
+  }
 });
 
 app.listen(process.env.PORT || 3001);
